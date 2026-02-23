b0dbadafd4cf3f2a9be9e32e14b93dd0ae79422e
tomorek-2 <omavrina89@gmail.com>
2026-02-23 10:58:52 +0300

    Update QwerWall.java

--- src/main/java/Tomodrek/QwerWall.java
+++ src/main/java/Tomodrek/QwerWall.java
@@ -3,15 +3,15 @@ package Tomodrek;
 import mindustry.world.blocks.defense.Wall;
 import mindustry.gen.Building;
 
-public class QwerWall extends Wall {
-    public QwerWall(String name) {
+public class qwerWall extends Wall {
+    public qwerWall(String name) {
         super(name);
   
   health = 450;
  size = 1;
     }
 
-    public class QwerWallBuild extends Building {
+    public class qwerWallBuild extends Building {
         @Override
         public void update() {
             super.update();
