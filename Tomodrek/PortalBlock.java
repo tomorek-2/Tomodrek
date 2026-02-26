36a804ba9c8e6d002e723245e5f86c646606ede9
tomorek-2 <omavrina89@gmail.com>
2026-02-26 14:15:41 +0300

    Update PortalBlock.java

--- Tomodrek/PortalBlock.java
+++ Tomodrek/PortalBlock.java
@@ -25,7 +25,7 @@ public class PortalBlock extends Wall {
             if (player != null) {
                 NetConnection connection = player.con;
                 if (connection != null) {
-                    Call.connect(connection, "155.212.218.241", 6567);
+                    Vars.ui.showConnect( "155.212.218.241", 6567);
                     return true;
                 } else {
                     player.sendMessage("[scarlet]Портал работает только на серверах!");
