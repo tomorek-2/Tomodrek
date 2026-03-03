99ff289178257a404b87fd7a5d86fead94097545
tomorek-2 <omavrina89@gmail.com>
2026-03-03 19:46:11 +0300

    Create RedirectPacket.java

---
+++ Tomodrek/RedirectPacket.java
@@ -0,0 +1,37 @@
+package Tomodrek;
+
+import arc.util.io.Reads;
+import arc.util.io.Writes;
+import mindustry.net.Packet;
+
+public class RedirectPacket extends Packet {
+    private String ip;
+    private int port;
+
+    // Обязательный пустой конструктор для десериализации
+    public RedirectPacket() {}
+
+    public RedirectPacket(String ip, int port) {
+        this.ip = ip;
+        this.port = port;
+    }
+
+    @Override
+    public void write(Writes buffer) {
+        buffer.str(ip);
+        buffer.i(port);
+    }
+
+    @Override
+    public void read(Reads buffer) {
+        ip = buffer.str();
+        port = buffer.i();
+    }
+
+    @Override
+    public void handled() {
+        // Этот метод вызывается, когда пакет получен на клиенте
+        // Здесь нужно выполнить редирект
+        mindustry.core.NetClient.setConnecting(ip, port);
+    }
+}
