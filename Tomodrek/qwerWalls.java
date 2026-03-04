d92faef52d02709326e050510830fd49534e1842
tomorek-2 <omavrina89@gmail.com>
2026-03-04 12:46:29 +0300

    Update qwerWalls.java

--- Tomodrek/qwerWalls.java
+++ Tomodrek/qwerWalls.java
@@ -8,7 +8,7 @@ import mindustry.world.meta.BuildVisibility;
 import mindustry.type.Item;        
 import mindustry.world.blocks.distribution.Router;
 
-
+import mindustry.world.blocks.defense.turrets;
 public class qwerWalls extends Mod {
     
     public static void load() {
