7db474deb15119ab9b2fbc64b18fccf629219c4f
tomorek-2 <omavrina89@gmail.com>
2026-03-06 12:36:39 +0300

    Update Modomodrek.java

--- Tomodrek/Modomodrek.java
+++ Tomodrek/Modomodrek.java
@@ -6,5 +6,9 @@ public class Modomodrek extends Mod {
     @Override
     public void loadContent() {
         qwerWalls.load();
+blocks.oreCopper.BuildVisibility = BuildVisibility.shown;
+blocks.oreCoal.BuildVisibilty = BuildVisibility.shown;
+blocks.oreLead.BuildVisibility = BuildVisibility.shown;
+blocks.sand.BuildVisibility = BuildVisibility.shown;
     }
 }
