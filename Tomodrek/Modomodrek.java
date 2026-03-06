67e1d40b60c81d0b6345242ccd8800c76cfb7d08
tomorek-2 <omavrina89@gmail.com>
2026-03-06 13:55:34 +0300

    Update Modomodrek.java

--- Tomodrek/Modomodrek.java
+++ Tomodrek/Modomodrek.java
@@ -5,6 +5,7 @@ import mindustry.world.meta.BuildVisibility;
 import mindustry.content.Blocks;
 import mindustry.content.Items;
 import mindustry.type.ItemStack;
+import mindustry.type.Category;
 
 public class Modomodrek extends Mod {
     @Override
@@ -12,10 +13,10 @@ public class Modomodrek extends Mod {
         qwerWalls.load();
 Blocks.oreCopper.buildVisibility = BuildVisibility.shown;
 Blocks.oreCoal.buildVisibility = BuildVisibility.shown;
-Blocks.oreCoal.requirements(ItemStack.with(Items.copper, 500, Items.lead, 500));
+Blocks.oreCoal.requirements(Category.defense, ItemStack.with(Items.copper, 500, Items.lead, 500));
 Blocks.oreLead.buildVisibility = BuildVisibility.shown;
 Blocks.sand.buildVisibility = BuildVisibility.shown;
-Blocks.sand.requirements(ItemStack.with(Items.copper, 500, Items.lead, 500, Items.coal, 450));
+Blocks.sand.requirements(Category.defense, ItemStack.with(Items.copper, 500, Items.lead, 500, Items.coal, 450));
 Blocks.oreTitanium.buildVisibility = BuildVisibility.shown;
 
 Blocks.removeOre.buildVisibility = BuildVisibility.shown; 
