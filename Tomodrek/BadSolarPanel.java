20c6e5e2048e2c6f49d7b5d98bd61221a97650f6
tomorek-2 <omavrina89@gmail.com>
2026-03-09 11:15:14 +0300

    New class: BadSolarPanel

---
+++ Tomodrek/BadSolarPanel.java
@@ -0,0 +1,40 @@
+package Tomodrek;
+import mindustry.game.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.type.*;
+import mindustry.type.unit.*;
+import mindustry.world.*;
+import mindustry.world.blocks.*;
+import mindustry.world.blocks.campaign.*;
+import mindustry.world.blocks.defense.*;
+import mindustry.world.blocks.defense.turrets.*;
+import mindustry.world.blocks.distribution.*;
+import mindustry.world.blocks.environment.*;
+import mindustry.world.blocks.heat.*;
+import mindustry.world.blocks.legacy.*;
+import mindustry.world.blocks.liquid.*;
+import mindustry.world.blocks.logic.*;
+import mindustry.world.blocks.payloads.*;
+import mindustry.world.blocks.power.*;
+import mindustry.world.blocks.production.*;
+import mindustry.world.blocks.sandbox.*;
+import mindustry.world.blocks.storage.*;
+import mindustry.world.blocks.units.*;
+import mindustry.world.consumers.*;
+import mindustry.world.draw.*;
+import mindustry.world.meta.*; 
+import mindustry.Vars.*;
+
+public class BadSolarPanel extends solarGenerator{
+  public BadSolarPanel(String name) {
+    super(name) 
+  } 
+public class BadSolarPanelBuild extends SolarGeneratorBuild{
+  @Override 
+  public void updateTile() {
+    damage(0.1f);
+  }
+} 
+} 
+
