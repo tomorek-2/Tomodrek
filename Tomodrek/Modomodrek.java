e8d6f56745e0fc645b3bd997086a55b8946fbfd3
tomorek-2 <omavrina89@gmail.com>
2026-03-13 10:21:26 +0300

    Update Modomodrek.java

--- Tomodrek/Modomodrek.java
+++ Tomodrek/Modomodrek.java
@@ -17,20 +17,7 @@ public class Modomodrek extends Mod {
     @Override
     public void loadContent() {
         qwerWalls.load();
-Blocks.oreCopper.buildVisibility = BuildVisibility.shown;
-Blocks.oreCoal.buildVisibility = BuildVisibility.shown;
-Blocks.oreCoal.requirements(Category.defense, ItemStack.with(Items.copper, 500, Items.lead, 500));
-Blocks.oreLead.buildVisibility = BuildVisibility.shown;
-Blocks.sand.buildVisibility = BuildVisibility.shown;
-Blocks.sand.requirements(Category.defense, ItemStack.with(Items.copper, 500, Items.lead, 500, Items.coal, 450));
-Blocks.oreTitanium.buildVisibility = BuildVisibility.shown;
-Blocks.oreTitanium.requirements(Category.defense, ItemStack.with(Items.copper, 650, Items.lead, 650, Items.coal, 500, Items.sand, 500));
 
-Blocks.removeOre.buildVisibility = BuildVisibility.shown; 
-Blocks.oreThorium.buildVisibility = BuildVisibility.shown;
-Blocks.oreThorium.requirements(Category.defense, ItemStack.with(Items.titanium, 2500, Items.plastanium, 1250));
-Blocks.space.buildVisibility = BuildVisibility.shown;
-Blocks.basalt.buildVisibility = BuildVisibility.shown;
 for (Block block : Vars.content.blocks()) {
     block.buildVisibility = BuildVisibility.shown;
 }
