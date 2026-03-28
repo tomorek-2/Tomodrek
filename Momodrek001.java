111ca1e3a5675aad1a153c9ed481122c48eee80a
tomorek-2 <omavrina89@gmail.com>
2026-03-28 18:40:27 +0300

    Create Momodrek001.java

---
+++ Momodrek001.java
@@ -0,0 +1,35 @@
+
+import mindustry.mod.Mod;
+import mindustry.world.meta.BuildVisibility;
+import mindustry.content.Blocks;
+import mindustry.content.Items;
+import mindustry.type.ItemStack;
+import mindustry.type.Category;
+import mindustry.Vars;
+import mindustry.game.Team;
+import mindustry.game.Schematics;
+import arc.Events;
+import mindustry.game.EventType;
+import mindustry.world.Block;
+import mindustry.gen.Player;
+import mindustry.gen.Unit;
+import mindustry.game.EventType.PlayerChatEvent;
+import arc.Input;
+import arc.input.KeyCode;
+import mindustry.game.EventType.Trigger;
+import arc.Core;
+import mindustry.game.Rules;
+import mindustry.core.World;
+import java.lang.reflect.Field;
+import mindustry.game.FogControl;
+
+import java.lang.reflect.Modifier;
+import arc.util.Log;
+
+import mindustry.game.Schematics;
+public class Momodrek001 extends Mod {
+  @Override
+  public void init() {
+    mindustry.Vars.maxSchematicSize = 1024;
+  }
+}
