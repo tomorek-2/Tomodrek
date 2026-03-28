b41217163b6f1147ac05e4a3997eebec9b06b666
tomorek-2 <omavrina89@gmail.com>
2026-03-28 21:11:25 +0300

    Update Modomodrek.java

--- Tomodrek/Modomodrek.java
+++ Tomodrek/Modomodrek.java
@@ -46,7 +46,7 @@ public class Modomodrek extends Mod {
 Field field0 = mindustry.game.Schematics.class.getDeclaredField("limitSchematicSize");
 field0.setAccessible(true);           
 field0.set(null, false);   
-        } catch {
+        } catch (){
         }
    
 
